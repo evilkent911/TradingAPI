99202ec305dc47d5fde6f3888a9e6a4e628ff295
Danny Su <danny.su@vantagefx.com>
2021-03-22 10:59:21 +1100

    Add dependencies

---
+++ InterviewQuestionTradingAPI/Controllers/SymbolsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MockTradingApi;
+
+namespace InterviewQuestionTradingAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SymbolsController : ControllerBase
+    {
+        private readonly ILogger<SymbolsController> _logger;
+
+        public SymbolsController(ILogger<SymbolsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Symbol> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
